20425a549e4b3a0900984df1a82d97b02c70098f
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-11-29 10:01:35 +0330

    Delete api/code.js

--- api/code.js
+++
@@ -1,8 +0,0 @@
-const clientID = 'YOUR_CLIENT_ID';
-const redirectURI = 'YOUR_REDIRECT_URI';
-const scope = 'SCOPE_REQUIRED'; // Scope specifying the permissions needed
-
-const authURL = `https://api.instagram.com/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scope}&response_type=code`;
-
-// Redirect the user to the authorization URL
-window.location.href = authURL;
