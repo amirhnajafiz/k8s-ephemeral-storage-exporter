5d74c769d95ba5bf8427c38bb2f8900a40872dac
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-05 17:46:02 +0330

    add: document inserts

--- api/posts.js
+++ api/posts.js
@@ -1,10 +1,15 @@
 // Function to get Instagram posts
 async function getPosts(accessToken) {
+  const el = document.getElementById('posts');
+
   try {
     const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink&access_token=${accessToken}`);
     const data = await response.json();
     console.log('Posts Information:', data);
+
+    el.innerHTML = data;
   } catch (error) {
     console.error('Error fetching posts:', error);
+    el.innerText = error;
   }
 }
