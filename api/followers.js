6936bca2265675fe9b94a2cd5d3e85152c258e66
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-05 17:21:01 +0330

    add: followers api

--- api/followers.js
+++ api/followers.js
@@ -1,11 +1,10 @@
-function getFollowers(accessToken) {
-  fetch(`https://graph.instagram.com/me/followers?access_token=${accessToken}`)
-  .then(response => response.json())
-  .then(data => {
-    // Process retrieved follower data
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// Function to get follower information
+async function getFollowers(accessToken) {
+  try {
+    const response = await fetch(`https://graph.instagram.com/me/followers?access_token=${accessToken}`);
+    const data = await response.json();
+    console.log('Follower Information:', data);
+  } catch (error) {
+    console.error('Error fetching followers:', error);
+  }
 }
