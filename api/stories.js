f0635302ee4fc6b3f9fc2ba4dadba93b18f83870
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-05 17:20:38 +0330

    add: stories api

--- api/stories.js
+++ api/stories.js
@@ -1,11 +1,10 @@
-function getStories(accessToken) {
-  fetch(`https://graph.instagram.com/me/stories?access_token=${accessToken}`)
-  .then(response => response.json())
-  .then(data => {
-    // Process retrieved Stories data
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// Function to get Instagram stories
+async function getStories(accessToken) {
+  try {
+    const response = await fetch(`https://graph.instagram.com/me/stories?fields=id,media_type,media_url,permalink&access_token=${accessToken}`);
+    const data = await response.json();
+    console.log('Stories Information:', data);
+  } catch (error) {
+    console.error('Error fetching stories:', error);
+  }
 }
