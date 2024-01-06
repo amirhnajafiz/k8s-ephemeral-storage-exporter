2db32f57e1e88d4bac1f9ecdf40d4e66a7a7585b
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-06 19:43:41 +0330

    add: main processor

--- hook/main.go
+++ hook/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type request struct {
@@ -30,7 +31,15 @@ func callback(host string, data []byte) error {
 	return nil
 }
 
-func process(r *request) {}
+func process(r *request) {
+	for i := 0; i < r.Data; i++ {
+		time.Sleep(1 * time.Second)
+	}
+
+	if err := callback(r.Host, []byte(fmt.Sprintf("server busy time: %ds", r.Data))); err != nil {
+		log.Println(fmt.Errorf("error in callback host=%s\n\t%w", r.Host, err))
+	}
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	process(nil)
