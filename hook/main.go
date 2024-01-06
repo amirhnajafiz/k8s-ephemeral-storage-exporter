3368574bf782736a83a73825198380f31529c8e5
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-06 19:34:47 +0330

    add: base methods

--- hook/main.go
+++ hook/main.go
@@ -3,16 +3,19 @@ package main
 import "net/http"
 
 type request struct {
-	Host string `json:"host"`
-	Data int    `json:"data"`
+	Host   string `json:"host"`
+	Method string `json:"method"`
+	Data   int    `json:"data"`
 }
 
-func process() {
-
+func callback() error {
+	return nil
 }
+
+func process(r *request) {}
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
+	process(nil)
 }
 
 func main() {
