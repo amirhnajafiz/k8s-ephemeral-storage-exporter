ead114c6024e9ce6817cdf638f06143ed6e80553
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-06 19:33:09 +0330

    add: main hook

---
+++ hook/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+type request struct {
+	Host string `json:"host"`
+	Data int    `json:"data"`
+}
+
+func process() {
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+
+}
