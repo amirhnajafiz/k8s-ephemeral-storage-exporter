827da32f8ef38f695fa7784ec988f4cf529f13f9
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-09 14:01:05 +0330

    init: main

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// TODO: load configurations
+	// TODO: load metrics
+	// TODO: connect to emqx cluster
+	// TODO: create a subscriber
+	// TODO: create a publisher
+}
