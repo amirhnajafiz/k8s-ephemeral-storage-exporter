abb6256b8c6197b299d547444f3ad32a0b0c4d72
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-09 14:35:55 +0330

    add: emqx client interface

--- internal/emqx/client.go
+++ internal/emqx/client.go
@@ -1,1 +1,8 @@
 package emqx
+
+import mqtt "github.com/eclipse/paho.mqtt.golang"
+
+type Client interface {
+	Publish(msg, topic string) mqtt.PublishToken
+	Subscribe(topic string) mqtt.SubscribeToken
+}
