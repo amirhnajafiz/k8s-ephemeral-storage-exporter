96dd765aa65ca9e6ee80bbaed75e85192756859b
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-11 10:03:15 +0330

    add: new method of mqtt client

--- internal/emqx/client.go
+++ internal/emqx/client.go
@@ -1,8 +1,44 @@
 package emqx
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	"log"
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 type Client interface {
 	Publish(msg, topic string) mqtt.PublishToken
 	Subscribe(topic string) mqtt.SubscribeToken
+}
+
+func New(cfg Config, callback mqtt.MessageHandler) (Client, error) {
+	// setting debug and error stdout
+	mqtt.DEBUG = log.New(os.Stdout, "emqx", 0)
+	mqtt.ERROR = log.New(os.Stdout, "emqx", 0)
+
+	// get mqtt connection options and
+	// create a new client
+	c := mqtt.NewClient(cfg.OPTs(callback))
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		return nil, token.Error()
+	}
+
+	return client{
+		cfg:  cfg,
+		conn: c,
+	}, nil
+}
+
+type client struct {
+	cfg  Config
+	conn mqtt.Client
+}
+
+func (c client) Publish(msg, topic string) mqtt.PublishToken {
+
+}
+
+func (c client) Subscribe(topic string) mqtt.SubscribeToken {
+
 }
--- internal/emqx/config.go
+++ internal/emqx/config.go
@@ -20,13 +20,14 @@ func (c Config) DNS() string {
 	return fmt.Sprintf("%s://%s:%d", c.Protocol, c.Host, c.Port)
 }
 
-func (c Config) OPTs() *mqtt.ClientOptions {
+func (c Config) OPTs(h mqtt.MessageHandler) *mqtt.ClientOptions {
 	opts := mqtt.NewClientOptions().
 		AddBroker(c.DNS()).
 		SetClientID(c.ClientID)
 
 	opts.SetKeepAlive(time.Duration(c.KeepAlive) * time.Second)
 	opts.SetPingTimeout(time.Duration(c.PingTimeout) * time.Second)
+	opts.SetDefaultPublishHandler(h)
 
 	return opts
 }
