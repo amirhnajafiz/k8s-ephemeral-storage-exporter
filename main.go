b352b93ffc0653689fddc6dfcd243f15abca79cb
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-11 10:25:05 +0330

    add: main load

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
+import (
+	"log"
+
+	"github.com/amirhnajafiz/emqx/internal/config"
+	"github.com/amirhnajafiz/emqx/internal/emqx"
+	"github.com/amirhnajafiz/emqx/internal/model"
+)
+
+func handler(channel chan *model.Message) {
+	go func() {
+		for {
+			msg := <-channel
+			log.Printf("topic:%s\n\t%s\n", msg.Topic, string(msg.Payload))
+		}
+	}()
+}
+
 func main() {
-	// TODO: load configurations
-	// TODO: load metrics
-	// TODO: connect to emqx cluster
-	// TODO: create a subscriber
-	// TODO: create a publisher
+	// load configurations
+	cfg := config.Load()
+
+	// create a channel
+	channel := make(chan *model.Message)
+
+	// connect to emqx cluster
+	client, err := emqx.New(cfg.EMQX, channel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create a new handler
+	handler(channel)
+
+	// subscribe on rides
+	client.Subscribe("rides")
+
+	// publish events
+	client.Publish("new-rider", "rides")
 }
