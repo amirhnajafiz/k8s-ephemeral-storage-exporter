b631211243ab0ddd404339f09269637cafc17fd5
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-11 10:11:57 +0330

    add: emqx client logic

--- internal/emqx/client.go
+++ internal/emqx/client.go
@@ -7,9 +7,12 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+// Client manages the three main methods of EMQX client
+// publishing, subscribing, and unsubscribing
 type Client interface {
-	Publish(msg, topic string) mqtt.PublishToken
-	Subscribe(topic string) mqtt.SubscribeToken
+	Publish(msg, topic string) mqtt.Token
+	Subscribe(topic string) mqtt.Token
+	UnSubscribe(topics ...string) mqtt.Token
 }
 
 func New(cfg Config, callback mqtt.MessageHandler) (Client, error) {
@@ -35,10 +38,14 @@ type client struct {
 	conn mqtt.Client
 }
 
-func (c client) Publish(msg, topic string) mqtt.PublishToken {
-
+func (c client) Publish(msg, topic string) mqtt.Token {
+	return c.conn.Publish(topic, 0, false, msg)
 }
 
-func (c client) Subscribe(topic string) mqtt.SubscribeToken {
+func (c client) Subscribe(topic string) mqtt.Token {
+	return c.conn.Subscribe(topic, 0, nil)
+}
 
+func (c client) UnSubscribe(topics ...string) mqtt.Token {
+	return c.conn.Unsubscribe(topics...)
 }
