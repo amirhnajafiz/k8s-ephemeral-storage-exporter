f0fea718f6f9186fc2b52bf89974c86970938607
amirhnajafiz <najafizadeh21@gmail.com>
2025-05-27 18:17:32 -0400

    update: summary model

--- internal/runner/model.go
+++ internal/runner/model.go
@@ -1,5 +1,6 @@
 package runner
 
+// summary represents the structure of the JSON response from the kubelet summary API.
 type summary struct {
 	Node struct {
 		NodeName string `json:"nodeName"`
@@ -8,7 +9,20 @@ type summary struct {
 		PodRef struct {
 			Name      string `json:"name"`
 			Namespace string `json:"namespace"`
+			UID       string `json:"uid"`
 		} `json:"podRef"`
+		Containers []struct {
+			Name   string `json:"name"`
+			Memory struct {
+				UsageBytes uint64 `json:"usageBytes"`
+			} `json:"memory"`
+			Rootfs struct {
+				UsedBytes uint64 `json:"usedBytes"`
+			} `json:"rootfs"`
+			Logs struct {
+				UsedBytes uint64 `json:"usedBytes"`
+			} `json:"logs"`
+		} `json:"containers"`
 		EphemeralStorage struct {
 			UsedBytes uint64 `json:"usedBytes"`
 		} `json:"ephemeral-storage"`
