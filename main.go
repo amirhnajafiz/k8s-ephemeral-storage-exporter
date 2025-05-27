75367832b0aca7dc94b6bb13d11429677a5bf2a9
amirhnajafiz <najafizadeh21@gmail.com>
2025-05-27 16:57:06 -0400

    add: main.go

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/amirhnajafiz/k8sese/internal/metrics"
+	"github.com/amirhnajafiz/k8sese/internal/runner"
+)
 
+func main() {
+	// create a new metrics instance
+	_, err := metrics.NewMetrics()
+	if err != nil {
+		panic(err)
+	}
+
+	// start the metrics server on port 8080
+	metrics.StartMetricsServer(8080)
+
+	// continue to collect metrics
+	runner.Start()
 }
