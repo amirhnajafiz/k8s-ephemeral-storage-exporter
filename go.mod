2a590d3e19a9d9e69114407bab3bcbbaed403c4b
amirhnajafiz <najafizadeh21@gmail.com>
2025-05-30 10:39:35 -0400

    add: zap logger

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/amirhnajafiz/k8sese
 
 go 1.24.1
 
-require github.com/prometheus/client_golang v1.22.0
+require (
+	github.com/prometheus/client_golang v1.22.0
+	go.uber.org/zap v1.27.0
+)
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -11,6 +14,7 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 )
--- internal/logr/zap.go
+++ internal/logr/zap.go
@@ -1,1 +1,25 @@
 package logr
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// NewZapLogger creates a new zap logger instance
+func NewZapLogger(debug bool) *zap.Logger {
+	// create a new zap logger instance based on the debug flag
+	var level zapcore.Level
+	if debug {
+		level = zapcore.DebugLevel
+	} else {
+		level = zapcore.InfoLevel
+	}
+
+	encoder := zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig())
+	core := zapcore.NewCore(encoder, zapcore.Lock(os.Stdout), level)
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+
+	return logger
+}
