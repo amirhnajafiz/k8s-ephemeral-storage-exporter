d85e117b1bb744257b03ab77e16f8c761b2fc75d
amirhnajafiz <najafizadeh21@gmail.com>
2025-05-30 10:28:32 -0400

    add: container metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -10,33 +10,52 @@ const (
 
 // Metrics holds the Prometheus metrics for the exporter.
 type Metrics struct {
-	storageUsageBytes *prometheus.GaugeVec
+	ephemeralStorageUsageBytes *prometheus.GaugeVec
+	containerStorageUsageBytes *prometheus.GaugeVec
 }
 
 // NewMetrics initializes and registers the Prometheus metrics for the exporter.
 func NewMetrics() (*Metrics, error) {
 	// create Prometheus metrics
-	sub := prometheus.NewGaugeVec(
+	esub := prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Namespace: NS,
 			Subsystem: SS,
 			Name:      "ephemeral_storage_usage_bytes",
 			Help:      "Ephemeral storage usage in bytes",
 		},
-		[]string{"pod", "namespace", "node", "container"},
+		[]string{"pod", "namespace", "node"},
+	)
+	cmub := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: NS,
+			Subsystem: SS,
+			Name:      "container_storage_usage_bytes",
+			Help:      "Container storage (memory, logs, and roofs) usage in bytes",
+		},
+		[]string{"pod", "namespace", "node", "container", "type"},
 	)
 
 	// register the metrics with Prometheus
-	if err := prometheus.Register(sub); err != nil {
+	if err := prometheus.Register(esub); err != nil {
+		return nil, err
+	}
+	if err := prometheus.Register(cmub); err != nil {
 		return nil, err
 	}
 
 	return &Metrics{
-		storageUsageBytes: sub,
+		ephemeralStorageUsageBytes: esub,
+		containerStorageUsageBytes: cmub,
 	}, nil
 }
 
-// SetStorageUsageBytes sets the ephemeral storage usage in bytes for a specific pod, namespace, node, and container.
-func (m *Metrics) SetStorageUsageBytes(pod, namespace, node, container string, value float64) {
-	m.storageUsageBytes.WithLabelValues(pod, namespace, node, container).Set(value)
+// SetStorageUsageBytes sets the ephemeral storage usage in bytes for a specific pod, namespace, and node.
+func (m *Metrics) SetStorageUsageBytes(pod, namespace, node string, value float64) {
+	m.ephemeralStorageUsageBytes.WithLabelValues(pod, namespace, node).Set(value)
+}
+
+// SetContainerStorageUsageBytes sets the storage usage in bytes for a specific container in a pod, namespace, and node.
+func (m *Metrics) SetContainerStorageUsageBytes(pod, namespace, node, container, typ string, value float64) {
+	m.containerStorageUsageBytes.WithLabelValues(pod, namespace, node, container, typ).Set(value)
 }
